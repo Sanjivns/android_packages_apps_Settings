fd5b85f43caed61a648b7ae3c12027d6d31ab24a
Sanjivns <alshahab109@gmail.com>
2025-01-30 17:57:05 +0000

    idk

--- src/com/android/settings/aospa/networktraffic/NetworkTrafficSettings.java
+++ src/com/android/settings/aospa/networktraffic/NetworkTrafficSettings.java
@@ -1,4 +1,4 @@
-package com.android.settings.pixelage.networktraffic;
+package com.android.settings.aospa.networktraffic;
 
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -90,7 +90,7 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment implement
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.PIXELAGE;
+        return MetricsProto.MetricsEvent.AOSPA;
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
